136f2b734516b8e621adcd748c105507cb33d5ae
Bruno Lima <brunoadl88@gmail.com>
2025-01-14 22:14:26 +0000

    Finishing Details Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,31 +1,54 @@
-import { Button } from "../../components/Button";
-import { Header } from "../../components/Header";
-import { Section } from "../../components/Section/index";
-import { Tag } from "../../components/Tag";
-import { Container, Links } from "./styles";
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section/index';
+import { Tag } from '../../components/Tag';
+import {
+  Container,
+  Content,
+  Links,
+} from './styles';
 
 export function Details() {
   return (
     <>
       <Container>
         <Header />
-        <Section title="Links uteis">
-          <Links>
-            <li>
-              <a href="#">https://rocketseat.com.br/</a>
-            </li>
-            <li>
-              <a href="#">https://rocketseat.com.br/</a>
-            </li>
-          </Links>
-        </Section>
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" />
 
-        <Section title="Marcadores">
-          <Tag title="express" />
-          <Tag title="nodejs" />
-        </Section>
+            <h1>Introducao ao React</h1>
 
-        <Button title="Voltar" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
+              sint corporis. Necessitatibus possimus rem aperiam libero,
+              sapiente voluptatum, vitae facilis doloribus, debitis quo enim
+              beatae ratione? Sapiente quod commodi totam? doloremque!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus
+              accusamus optio hic corporis, laborum eligendi at vel fugit, error
+              impedit expedita, laboriosam delectus beatae. Est nisi magnam
+              magni repellendus
+            </p>
+            <Section title="Links uteis">
+              <Links>
+                <li>
+                  <a href="#">https://rocketseat.com.br/</a>
+                </li>
+                <li>
+                  <a href="#">https://rocketseat.com.br/</a>
+                </li>
+              </Links>
+            </Section>
+            <Section title="Marcadores">
+              <Tag title="express" />
+              <Tag title="nodejs" />
+            </Section>
+            <Button title="Voltar" />
+          </Content>
+        </main>
       </Container>
     </>
   );
