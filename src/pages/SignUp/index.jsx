d2b86fdd1c236e36ab40726fccf26a92842b3ce8
Bruno Lima <brunoadl88@gmail.com>
2025-02-03 00:37:21 +0000

    Connecting API with Axios

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,21 +1,66 @@
+import { useState } from "react";
+
 import { FiLock, FiMail, FiUser } from "react-icons/fi";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { api } from "../../services/api";
 import { Background, Container, Form } from "./styles";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Fill in all fields!");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("User registered successfully!");
+
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Unable to register");
+        }
+      });
+  }
+
   return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
         <p>An application to save and manager your usefull links.</p>
         <h2>Create an account</h2>
-        <Input placeholder="Username" type="text" icon={FiUser} />
-        <Input placeholder="Email" type="text" icon={FiMail} />
-        <Input placeholder="Password" type="password" icon={FiLock} />
-        <Button title="Sign up" />
+        <Input
+          placeholder="Username"
+          type="text"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Email"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button title="Sign up" onClick={handleSignUp} />
 
         <Link to="/">Already have an account?</Link>
       </Form>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter } from "react-router";
 
-import { AppRoutes } from "./app.routes";
-
-// import { AuthRoutes } from "./auth.routes";
+// import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AppRoutes />
-      {/* <AuthRoutes /> */}
+      {/* <AppRoutes /> */}
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
